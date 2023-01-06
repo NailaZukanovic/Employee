e98fbb48f9a5d1e82dcc5faa8f53556c45b41b74
Naila <nailazukanovic@gmail.com>
2023-01-06 17:49:43 +0100

    initial commit

--- config/db.config.js
+++ config/db.config.js
@@ -6,7 +6,7 @@ const mysql = require('mysql');
 const dbConn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Brsoft@123',
+  password : '',
   database : 'node_mysql_crud_db'
 });
 dbConn.connect(function(err) {
